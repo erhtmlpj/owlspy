e7c5f22797e3cd97e9d16444b546fa7bd225c012
erhtmlpj <htmlpj.er@gmail.com>
2017-05-07 14:07:18 -0400

    Projeto 0.0.4
    Adicionada tela de instruções da ferramenta, tela de login, e script em javascript.

---
+++ Projeto 0.0.4/mainScript.js
@@ -0,0 +1,11 @@
+var isLogged = true;
+if(isLogged)
+{
+    document.write("<aside>");
+    document.write("<ul>");
+    document.write("<li><a>Administracao</a></li>");
+    document.write("<li><a>Logout</a></li>");
+    document.write("<li><a>Minha Conta</a></li>");
+    document.write("<ul>");
+    document.write("</aside>");
+}
